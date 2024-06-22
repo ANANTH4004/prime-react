96a978a6258680ce0786229ad961126adce10d3a
ANANTH4004 <balamuruganananth@gmail.com>
2024-06-22 23:08:50 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting() {
+  return (
+    <>
+      <h1>Hello World11!</h1>
+      <Person />
+      <Message />
+    </>
+  );
+}
+export default Greeting;
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Greeting />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Person = () => <h2>Hello from Anand1!</h2>;
+const Message = () => {
+  return (
+    <p className='message'>Now i am trying nested component concept in react</p>
+  );
+};
