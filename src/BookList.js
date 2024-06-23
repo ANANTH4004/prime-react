56004b19765000414497a0ec9fad501eb24f4475
ANANTH4004 <balamuruganananth@gmail.com>
2024-06-23 21:14:52 +0530

    simple loop using map and props in component

--- src/BookList.js
+++ src/BookList.js
@@ -1,28 +1,35 @@
 import './styles/index.css';
+const bookList = [
+  {
+    author: 'Anand',
+    title: 'Catch me If You Can',
+    imgPath: '../public/logo192.png',
+  },
+  {
+    author: 'varun',
+    title: 'Ninaivellam Nithya',
+    imgPath: '../public/logo152.png',
+  },
+];
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        author='Anand'
-        title='Catch me If You Can'
-        imgPath='../public/logo192.png'
-      />
-      <Book
-        author='Varun'
-        title='Ninaivellam Nithya'
-        imgPath='../public/logo152.png'
-      />
+      {bookList.map((book, index) => {
+        return <Book book={book} key={index} />;
+      })}
     </section>
   );
 }
 
 export default BookList;
 const Book = (props) => {
+  const { title, author, imgPath } = props.book;
   return (
     <article className='book'>
-      <h1>{props.title}</h1>
-      <img src={props.imgPath} alt='Here will be the Book Image' />
-      <p className='author-name'>{props.author}</p>
+      <h1>{title}</h1>
+      <img src={imgPath} alt='Here will be the Book Image' />
+      <p className='author-name'>{author}</p>
     </article>
   );
 };
