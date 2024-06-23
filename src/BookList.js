7e2bfeb43677456b7503d255e842f2771401d454
ANANTH4004 <balamuruganananth@gmail.com>
2024-06-23 20:15:10 +0530

    used props in component

--- src/BookList.js
+++ src/BookList.js
@@ -2,22 +2,27 @@ import './styles/index.css';
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author='Anand'
+        title='Catch me If You Can'
+        imgPath='../public/logo192.png'
+      />
+      <Book
+        author='Varun'
+        title='Ninaivellam Nithya'
+        imgPath='../public/logo152.png'
+      />
     </section>
   );
 }
 
 export default BookList;
-const Book = () => {
+const Book = (props) => {
   return (
     <article className='book'>
-      <h1>This is the Title</h1>
-      <img src='' alt='Here will be the Book Image' />
-      <p className='author-name'>Anand</p>
+      <h1>{props.title}</h1>
+      <img src={props.imgPath} alt='Here will be the Book Image' />
+      <p className='author-name'>{props.author}</p>
     </article>
   );
 };
